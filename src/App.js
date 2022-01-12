8350ca3c3355c36cf076c1f41899fc93f6294454
Geraldoeze <egerald344@gmail.com>
2022-01-12 18:54:40 +0100

    added favourite

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import MovieList from './components/MovieList';
 import MovieHeading from './components/MovieHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourite from './components/AddFavourites';
+import RemoveFavourite from './components/RemoveFavou';
+
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -28,16 +30,33 @@ const App = () => {
   useEffect(() => {
     getMovieRequest(searchValue);
     console.log('change', searchValue, movies)
-  }, [searchValue]); 
+  }, [searchValue]);
+  
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites'));
+    setFavourite(movieFavourites)
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  }
 
   const AddFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourite, movie];
     setFavourite(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   }
 
   const changeValue = (val) => {
     setSearchValue(val)
   }
+
+const RemoveFavouriteMovie = (movie) => {
+  const newFavouriteList = favourite.filter((favour) => favour.imdbID !== movie.imdbID);
+  setFavourite(newFavouriteList)
+  saveToLocalStorage(newFavouriteList);
+}
+
   return ( 
     <div className='container-fluid movie-app'>
     <div className=' d-flex align-items-center mt-4 mb-4'>
@@ -49,6 +68,15 @@ const App = () => {
             movies={movies} 
             handleFavouritesClick={AddFavouriteMovie}
             favouriteComponent={AddFavourite}/>
+      </div>
+      <div className=' d-flex align-items-center mt-4 mb-4'>
+        <MovieHeading heading="Favourites"/>
+      </div>
+      <div className='row'>
+          <MovieList  
+            movies={favourite} 
+            handleFavouritesClick={RemoveFavouriteMovie}
+            favouriteComponent={RemoveFavourite}/>
       </div>
     </div>
    );
---
+++ src/components/RemoveFavou.js
@@ -0,0 +1,15 @@
+
+
+const RemoveFavourite = () => {
+    return ( 
+        <>
+        <span className="mr-3">Remove from Favourites</span>
+        <svg
+            width=''>
+
+        </svg>
+        </>
+     );
+}
+ 
+export default RemoveFavourite;
